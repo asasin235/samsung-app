cd536137e379b66de3ad44fe58cd629b193956c3
aatif <aatif187899@st.jmi.ac.in>
2022-02-27 13:41:49 +0530

    added login work

---
+++ app/src/main/java/com/samsung/app_asignment/CalculatorActivity.java
@@ -0,0 +1,14 @@
+package com.samsung.app_asignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+}
